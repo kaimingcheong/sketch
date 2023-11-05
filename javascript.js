7ece0e7e7abae1ea1988cad3929f291325ab4ff8
kaiming <kaiming@hotmail.co.nz>
2023-11-05 22:23:33 +1300

    update

--- javascript.js
+++ javascript.js
@@ -1,19 +1,21 @@
-function makeGrid(length){
-    //Can use this but for accuracy we can use ID's 
-    //const e = document.grid; or alternatively 
-    //const grid = document.getElementById("grid")
+function createGrid(size){
+    let grid = document.querySelector(".grid");
+    let squares = grid.querySelectorAll("div");
+    squares.forEach((div) => div.remove());
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-    const grid = document.querySelector('#grid');
-    
-    for (var i = 0; i < length; i++){
-        var row = document.createElement('div');
-        row.className = 'row';
-        for (var j = 1; j <= length; j++){
-            var cell = document.createElement('div');
-            cell.className('gridSquare');
-            cell.innerText = (i * length) + j;
-            row.appendChild(cell);
-        }
-        grid.appendChild(row);
+    for (let i = 0; i < (size*size); i++){
+        let square = document.createElement("div");
+        square.addEventListener("mouseover", colorSquare)
+        square.style.backgroundColor = "white";
+        grid.insertAdjacentElement("beforeend", square);
     }
 }
+
+createGrid(16);
+
+function colorSquare(){
+    this.style.backgroundColor = "black";
+}
+ 
